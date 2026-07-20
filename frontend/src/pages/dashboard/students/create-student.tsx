c3631c05877e0dc2b69d6fa9719c923adce0d176
ahmed saleban <ahmedsalebaancrtan@gmail.com>
2026-07-20 13:50:58 +0300

    done

--- frontend/src/pages/dashboard/students/create-student.tsx
+++ frontend/src/pages/dashboard/students/create-student.tsx
@@ -1,8 +1,16 @@
+import ComponentHeader from "@/components/utilits/component-header"
 
 
 const  CreateSTudent = () => {
   return (
-    <div>create-student</div>
+    <div>
+      <ComponentHeader
+        title="Student Create form"
+        description="Manage  and create students"
+        to="/dashboard/students"
+        btnText="Back to list"
+      />
+    </div>
   )
 }
 
